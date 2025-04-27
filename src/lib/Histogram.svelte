aa93e5b6df774aa83c8f5c378bbbaf7af7a4f129
Greg Graf <me@greggraf.com>
2025-04-27 12:01:02 -0400

    make histogram squatter and relatively sized

--- src/lib/Histogram.svelte
+++ src/lib/Histogram.svelte
@@ -1,5 +1,5 @@
 <script>
-	const histogramHeight = 156;
+	const histogramHeight = 116;
 
 	function scaleValue(value, max = 255) {
 		return Math.floor((value / max) * histogramHeight);
@@ -23,43 +23,54 @@
 </script>
 
 {#if greyscaleImageData.length > 0}
-	<svg width="256" height={histogramHeight} class="container">
-		<g class="isolator">
-			<g fill="black">
-				{#each histogram as bar, i}
-					<rect x={i * 1} y={histogramHeight - bar} width="1" height={bar} />
-				{/each}
+	<div>
+		<svg
+			width="100%"
+			height="100%"
+			x="0"
+			y="0"
+			preserveAspectRatio="none"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 256 {histogramHeight}"
+			class="container"
+		>
+			<g class="isolator">
+				<g fill="black">
+					{#each histogram as bar, i}
+						<rect x={i * 1} y={histogramHeight - bar} width="1" height={bar} />
+					{/each}
+				</g>
+				<g fill="black">
+					<rect
+						x="0"
+						y="0"
+						width={blackpoint}
+						height={histogramHeight}
+						fill="black"
+						class="overlay"
+					/>
+				</g>
+				<g fill={hexColor}>
+					<rect
+						x={blackpoint}
+						y="0"
+						width={whitepoint - blackpoint}
+						height={histogramHeight}
+						class="overlay"
+					/>
+				</g>
+				<g fill="#fff">
+					<rect
+						x={whitepoint}
+						y="0"
+						width={256 - whitepoint}
+						height={histogramHeight}
+						class="overlay"
+					/>
+				</g>
 			</g>
-			<g fill="black">
-				<rect
-					x="0"
-					y="0"
-					width={blackpoint}
-					height={histogramHeight}
-					fill="black"
-					class="overlay"
-				/>
-			</g>
-			<g fill={hexColor}>
-				<rect
-					x={blackpoint}
-					y="0"
-					width={whitepoint - blackpoint}
-					height={histogramHeight}
-					class="overlay"
-				/>
-			</g>
-			<g fill="#fff">
-				<rect
-					x={whitepoint}
-					y="0"
-					width={256 - whitepoint}
-					height={histogramHeight}
-					class="overlay"
-				/>
-			</g>
-		</g>
-	</svg>
+		</svg>
+	</div>
 {/if}
 
 <style>
@@ -74,5 +85,8 @@
 	}
 	.isolator {
 		isolatation: isolate;
+	}
+	svg {
+		width: 100%;
 	}
 </style>
