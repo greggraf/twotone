55eb9f30bde58c94b6ee323347d7c65df29c7d6b
Greg Graf <me@greggraf.com>
2025-05-26 19:05:29 -0400

    update readme, add some comments to deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,11 @@
 import { exec } from 'child_process';
 
+// This script deploys the build to the server
+// It expcets a DEPLOYUSER and DEPLOYHOST environment variable
+// And it uses rsync to sync the build to the server
+// I also set up key based permissions for running the rsync command without having to enter a password
+// great, I also assume you want the build to be deployed to scratch/twotone I guess i should change that
+
 const deployUser = process.env.DEPLOYUSER;
 const deployHost = process.env.DEPLOYHOST;
 const deployCommand = `rsync -avz build/ ${deployUser}:${deployHost}/scratch/twotone/`;
